fcad85519d995eae32e9eed22a29ef9ccb4003ee
Chloe Nott <chloe@nott.email>
2021-11-30 14:49:17 -0800

    Added test for a list of first 13 fibonacci numbers. Uses for loop to assert for each index in the loop. Counts as one test for all the numbers in the list. Test passes. Fibonacci function seems to work fine

--- tests/test_series.py
+++ tests/test_series.py
@@ -39,3 +39,10 @@ def test_fib6():
     actual = fibonacci(6)
     expected = 8
     assert actual == expected
+
+def test_fibMany():
+    n_list = [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144]
+    for i in range(len(n_list)):
+        actual = fibonacci(i)
+        expected = n_list[i]
+        assert actual == expected
